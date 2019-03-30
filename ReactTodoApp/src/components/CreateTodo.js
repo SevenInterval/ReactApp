2df725112b64b821499f234745171742a1f1a6dc
SevenInterval <ridvanozturk94@gmail.com>
2019-03-30 13:47:20 +0300

    Create Todo App

--- ReactTodoApp/src/components/CreateTodo.js
+++ ReactTodoApp/src/components/CreateTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateTodo extends Component {
     constructor(props) {
@@ -18,12 +19,22 @@ export default class CreateTodo extends Component {
 
     onSubmit(e) {
         e.preventDefault();
-        
+
         console.log(`Form submitted:`);
         console.log(' Description: ' + this.state.description);
         console.log(' Responsible: ' + this.state.responsible);
         console.log(' Priority: ' + this.state.priority);
-        
+
+        const newTodo = {
+            description: this.state.description,
+            responsible: this.state.responsible,
+            priority: this.state.priority,
+            completed: this.state.completed
+        }        
+
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => console.log(res.data));
+
         this.setState({
             description: '',
             responsible: '',
@@ -51,58 +62,58 @@ export default class CreateTodo extends Component {
     }
     render() {
         return (
-            <div style={{marginTop: 10}}>
+            <div style={{ marginTop: 10 }}>
                 <h3>Create New Todo</h3>
                 <form onSubmit={this.onSubmit}>
-                    <div className="form-group"> 
+                    <div className="form-group">
                         <label>Description: </label>
-                        <input  type="text"
-                                className="form-control"
-                                value={this.state.description}
-                                onChange={this.onChangeDescription}
-                                />
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                        />
                     </div>
                     <div className="form-group">
                         <label>Responsible: </label>
-                        <input 
-                                type="text" 
-                                className="form-control"
-                                value={this.state.responsible}
-                                onChange={this.onChangeResponsible}
-                                />
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.responsible}
+                            onChange={this.onChangeResponsible}
+                        />
                     </div>
                     <div className="form-group">
                         <div className="form-check form-check-inline">
-                            <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityLow" 
-                                    value="Low"
-                                    checked={this.state.priority==='Low'} 
-                                    onChange={this.onChangePriority}
-                                    />
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityLow"
+                                value="Low"
+                                checked={this.state.priority === 'Low'}
+                                onChange={this.onChangePriority}
+                            />
                             <label className="form-check-label">Low</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityMedium" 
-                                    value="Medium" 
-                                    checked={this.state.priority==='Medium'} 
-                                    onChange={this.onChangePriority}
-                                    />
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityMedium"
+                                value="Medium"
+                                checked={this.state.priority === 'Medium'}
+                                onChange={this.onChangePriority}
+                            />
                             <label className="form-check-label">Medium</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityHigh" 
-                                    value="High" 
-                                    checked={this.state.priority==='High'} 
-                                    onChange={this.onChangePriority}
-                                    />
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityHigh"
+                                value="High"
+                                checked={this.state.priority === 'High'}
+                                onChange={this.onChangePriority}
+                            />
                             <label className="form-check-label">High</label>
                         </div>
                     </div>
